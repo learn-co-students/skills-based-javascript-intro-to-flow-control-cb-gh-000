fcc91be2fdd3339953281b7b6ce62e5d5fa6b55b
Tibor Schöll <scti1016@hs-karlsruhe.de>
2017-03-22 20:11:26 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,56 @@
+
+
 function basicTeenager(age) {
+
+if (age >12 && age < 20 ) {
+return "You are a teenager!";
+}
+else {
+  return ;
+}
 
 }
 
 function teenager(age) {
 
+if (basicTeenager(age)){
+return basicTeenager(age)
+}
+else {
+   return "You are not a teenager";
+}
 }
 
 function ageChecker(age) {
-
+if (basicTeenager(age))
+return basicTeenager(age);
+if ( age <= 12){
+return "You are a kid";
+}
+if (age >= 20){
+return "You are a grownup";
+}
 }
 
 function ternaryTeenager(age) {
 
+  if (teenager(age)){
+    return teenager(age);
+  }
+  else {
+    return "You are not a teenager";
+  }
+
 }
 
 function switchAge(age) {
+
+  if (!(age >=13 && age <=19)){
+    return "You have an age";
+  }
+  else {
+    return basicTeenager(age);
+  }
+
 
 }
