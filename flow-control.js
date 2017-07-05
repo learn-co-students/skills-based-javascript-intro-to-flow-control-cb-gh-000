eb09bfe5187ea5dfe6dbf5b2ae2a09b79bc703fa
Govind Menon <govindmenon1995@yahoo.co.in>
2017-07-05 11:31:54 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,37 @@
 function basicTeenager(age) {
-
+	if(age<=19 && age>=13)
+		 return "You are a teenager!";	
 }
 
 function teenager(age) {
-
+	var basic = basicTeenager(age);
+	if(basic != undefined)
+		return basic;
+	else
+		return "You are not a teenager";
 }
 
 function ageChecker(age) {
-
+	var basic = basicTeenager(age);
+        if(basic != undefined)
+                return basic;
+        else if(age<=12)
+		return "You are a kid";
+	else
+	return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
-
+	var ret = teenager(age);
+	if(ret.endsWith("!"))
+	ret = ret.substring(0,ret.length-1);
+	return ret;
 }
 
 function switchAge(age) {
-
+	var basic = basicTeenager(age);
+        if(basic != undefined)
+                return basic.substring(0,basic.length-1);
+        else
+                return "You have an age";
 }
