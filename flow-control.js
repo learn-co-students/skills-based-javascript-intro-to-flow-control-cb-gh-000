a4a94893d2364cdefc1b39368e1d4ebc77b6b566
Rayana23 <sanzanarayana@gmail.com>
2017-08-13 23:55:48 +0800

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,40 @@
 function basicTeenager(age) {
-
+  if (age === 13 || age === 14 || age === 15 || age === 16 || age === 17 || age === 18) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
+  if (age === 13 || age === 14 || age === 15 || age === 16 || age === 17 || age === 18) {
+    return "You are a teenager!";
+  } else  {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if (age === 13 || age === 14 || age === 15 || age === 16 || age === 17 || age === 18) {
+    return 'You are a teenager!';
+  } else if(age <= 12) {
+    return 'You are a kid'
+  } else {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-
+  if (age === 13 || age === 14 || age === 15 || age === 16 || age === 17 || age === 18) {
+    return 'You are a teenager';
+  } else  {
+    return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
+  if (age === 13 || age === 14 || age === 15 || age === 16 || age === 17 || age === 18) {
+    return 'You are a teenager';
+  } else {
+    return "You have an age"
+  }
 
 }
