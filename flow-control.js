8c769429463ba2b5668f2001d83baea5bde2f528
Raphael Schinköth <rapha@schinkoeth.com>
2018-02-12 17:42:29 +0100

    done

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,47 @@
 function basicTeenager(age) {
-
+    if (age <= 19 && age >= 13) {
+        return "You are a teenager!"
+    }
 }
 
 function teenager(age) {
-
+    if (age <= 19 && age >= 13) {
+        return "You are a teenager!"
+    }
+    else {
+        return "You are not a teenager"
+    }
 }
 
 function ageChecker(age) {
+    if (age <= 19 && age >= 13) {
+        return "You are a teenager!"
+    }
+    else if (age <= 12) {
+        return "You are a kid"
+    }
+    if (age >= 20) {
+        return "You are a grownup"
+    }
 
 }
 
 function ternaryTeenager(age) {
-
+    if (age <= 19 && age >= 13) {
+        return "You are a teenager"
+    }
+    else {
+        return "You are not a teenager"
+    }
 }
 
 function switchAge(age) {
+    switch (age) {
+        case 15:
+            return "You are a teenager"
+        default:
+            return "You have an age"
+
+    }
 
 }
