caf0c214aed440f2c4d7ff10337a668afbb1ab24
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-16 02:56:49 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -42,6 +42,21 @@ describe('flow-control', () => {
       expect(teenager(29)).toEqual("You are not a teenager")
     })
   })
+  function teenager(age){
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!";
+  }
+  else if(age <= 12){
+    return "You are a kid";
+  }
+  else if(age >= 20){
+    retturn "You are a grownup!"
+  }
+  else {
+    return "You are not a teenager";
+  }
+}
+teenager(20);
 
   describe('ageChecker', () => {
     it('should return "You are a teenager!" if the age is between 13-19', () => {
@@ -78,3 +93,39 @@ describe('flow-control', () => {
     })
   })
 })
+function basicTeenager(age) {
+  if(age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
+}
+
+function teenager(age) {
+  if(age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
+}
+
+function ageChecker(age) {
+  if(age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  } else if(age <= 12) {
+    return "You are a kid"
+  } else if(age > 19) {
+    return "You are a grownup"
+  }
+}
+
+function ternaryTeenager(age) {
+  return age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
+}
+
+function switchAge(age) {
+  switch(age <= 19 && age >= 13) {
+    case true :
+    return "You are a teenager"
+    default:
+    return "You have an age"
+  }
+}
