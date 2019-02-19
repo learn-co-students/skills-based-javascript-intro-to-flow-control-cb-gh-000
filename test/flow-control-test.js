953eb16846d8c3d7d8309234dd8ce2f237c19fc3
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-19 07:59:35 +0000

    Don't know the reason why I could not pass the test

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -50,7 +50,7 @@ describe('flow-control', () => {
     return "You are a kid";
   }
   else if(age >= 20){
-    retturn "You are a grownup!"
+    return "You are a grownup!"
   }
   else {
     return "You are not a teenager";
@@ -93,39 +93,3 @@ teenager(20);
     })
   })
 })
-function basicTeenager(age) {
-  if(age >= 13 && age <= 19) {
-    return "You are a teenager!"
-  }
-}
-
-function teenager(age) {
-  if(age >= 13 && age <= 19) {
-    return "You are a teenager!"
-  } else {
-    return "You are not a teenager"
-  }
-}
-
-function ageChecker(age) {
-  if(age >= 13 && age <= 19) {
-    return "You are a teenager!"
-  } else if(age <= 12) {
-    return "You are a kid"
-  } else if(age > 19) {
-    return "You are a grownup"
-  }
-}
-
-function ternaryTeenager(age) {
-  return age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
-}
-
-function switchAge(age) {
-  switch(age <= 19 && age >= 13) {
-    case true :
-    return "You are a teenager"
-    default:
-    return "You have an age"
-  }
-}
