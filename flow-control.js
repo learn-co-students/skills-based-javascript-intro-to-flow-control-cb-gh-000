312424eaf87549de1414ff436fff300608330a46
Andrea Spadavecchia <spadavecchia.andrea@spes.uniud.it>
2019-05-04 21:03:24 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,11 @@
 function basicTeenager(age) {
-  if (age >= 13 && <= 19) {
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!"
   }
 }
 
 function teenager(age) {
-  if (age >= 13 && <= 19) {
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!"
   }
   return "You are not a teenager"
@@ -13,9 +13,9 @@ function teenager(age) {
 
 function ageChecker(age) {
   if (age <= 12 ) {
-    return "You are a kid":
-  } else if (age > 12 || age <= 19) {
-    return "you are a teenager";
+    return "You are a kid"
+  } else if (age > 12 && age <= 19) {
+    return "You are a teenager!";
   } else {
     return "You are a grownup";
   }
@@ -26,14 +26,16 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  case 13:
-  case 14:
-  case 15:
-  case 16:
-  case 17:
-  case 18:
-  case 19:
-    return "You are a teenager";
-  default:
-   return "You are not a teenager";
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager";
+    default:
+     return "You have an age";
+   }
 }
