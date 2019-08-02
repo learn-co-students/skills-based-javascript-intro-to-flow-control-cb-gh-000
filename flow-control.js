8bf76d528cea1a2eec2da2aba01fe190aea67b34
Duong My Phuong <kho.design113@gmail.com>
2019-08-02 00:38:14 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,39 @@
 function basicTeenager(age) {
-
+ if( 19 >= age && age >= 13) {
+   return "You are a teenager!"
+ }
 }
 
 function teenager(age) {
-
+  if( 19 >= age && age >= 13) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if( 19 >= age && age >= 13) {
+    return "You are a teenager!"
+  } else if ( 12>= age ) {
+    return "You are a kid"
+  } else {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-
+  if( 19 >= age && age >= 13) {
+    return "You are a teenager"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
-
+  if( 19 >= age && age >= 13) {
+    return "You are a teenager"
+  } else {
+    return "You have an age"
+  }
 }
